7270aee90eefd846b03e7411536c2e8a3f9a6b68
Cal Schwick <35741152+CalSch@users.noreply.github.com>
2024-12-10 15:08:54 +0000

    made convert.py do stuff

--- convert.py
+++ convert.py
@@ -1,16 +1,22 @@
 import sys
 from PIL import Image
 import os
+from real_webp import RealWebP
 
-if len(sys.argv)<2:
-    print("put in a filename bozo")
+#TODO: use a real argv parser
+if len(sys.argv)<3:
+    print("put in two filenames bozo (in and out files)")
     exit(1)
 
-fname = sys.argv[1]
+in_file = sys.argv[1]
+out_file = sys.argv[2]
+rwp = RealWebP(0,0)
 
-#TODO: do something
-if fname.split(".")[-1]=="rwp":
-    pass
-else:
-    pass
+#TODO: make this better
+if in_file.split(".")[-1]=="rwp": # Convert from RWP to other
+    rwp.load(in_file)
+    rwp.save_to_img(out_file)
+else: # Convert from other to RWP
+    rwp.load_from_img(in_file)
+    rwp.save(out_file)
 
